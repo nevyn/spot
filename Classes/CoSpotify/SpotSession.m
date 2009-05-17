d21d7837b6f45d3d2475d2e5cc8f636532b2e352
Joachim Bengtsson <joachimb@gmail.com>
2009-05-18 00:44:43 +0200

    print info; add the simple client binary

--- Classes/CoSpotify/SpotSession.m
+++ Classes/CoSpotify/SpotSession.m
@@ -8,10 +8,38 @@
 
 #import "SpotSession.h"
 #import "SpotPlaylist.h"
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <wchar.h>
+
 
 SpotSession *SpotSessionSingleton;
 
 NSString *SpotSessionErrorDomain = @"SpotSessionErrorDomain";
+
+static void print_info(struct despotify_session* ds)
+{
+    struct user_info* user = ds->user_info;
+    wprintf(L"Username       : %s\n", user->username);
+    wprintf(L"Country        : %s\n", user->country);
+    wprintf(L"Account type   : %s\n", user->type);
+    wprintf(L"Account expiry : %s", ctime(&user->expiry));
+    wprintf(L"Host           : %s:%d\n", user->server_host, user->server_port);
+    wprintf(L"Last ping      : %s", ctime(&user->last_ping));
+	
+    if (strncmp(user->type, "premium", 7)) {
+        wprintf(L"\n=================================================\n"
+				"                  N O T I C E\n"
+				"       You do not have a premium account.\n"
+				"     Spotify services will not be available.\n"
+				"=================================================\n");
+    }
+}
+
 
 @implementation SpotSession
 +(SpotSession*)defaultSession;
@@ -26,9 +54,18 @@ NSString *SpotSessionErrorDomain = @"SpotSessionErrorDomain";
 {
 	if( ! [super init] ) return nil;
 	
-	despotify_init();
+	if(!despotify_init()) {
+		NSLog(@"Init failed");
+		[self release];
+		return nil;
+	}
 	
 	session = despotify_init_client();
+	if( !session) {
+		NSLog(@"Init client failed");
+		[self release];
+		return nil;
+	}
 	
 	return self;
 }
@@ -51,8 +88,11 @@ NSString *SpotSessionErrorDomain = @"SpotSessionErrorDomain";
 	BOOL success = despotify_authenticate(session, [user UTF8String], [password UTF8String]);
 	if(!success && error)
 		*error = [NSError errorWithDomain:SpotSessionErrorDomain code:SpotSessionErrorCodeDefault userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"%s", despotify_get_error(session)] forKey:NSLocalizedDescriptionKey]];
-	if(success)
+	usleep(500000);
+	if(success) {
+		print_info(session);
 		NSLog(@"Successfully logged in as %@", user);
+	}	
 	return success;
 }
 
