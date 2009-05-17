4d2b74a13c1926e8f3a9813b2114f827f89afa2c
Joachim Bengtsson <joachimb@gmail.com>
2009-05-18 01:15:06 +0200

    2.2.1 compat

--- Classes/PlaylistsViewController.m
+++ Classes/PlaylistsViewController.m
@@ -92,7 +92,7 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
     }
     
 	// Configure the cell.
