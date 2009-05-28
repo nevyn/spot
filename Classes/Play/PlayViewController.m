9acc8af09b1a58391cb68b2b98d630730b6dc498
Patrik Sjöberg <voxxar@vMac.local>
2009-05-28 16:18:06 +0200

    kalaspuff's Audiosessioncode

--- Classes/Play/PlayViewController.m
+++ Classes/Play/PlayViewController.m
@@ -11,6 +11,9 @@
 #import "SpotTrack.h"
 #import "SpotArtist.h"
 
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
 PlayViewController *GlobalPlayViewController;
 
 @interface PlayViewController ()
@@ -29,6 +32,11 @@ PlayViewController *GlobalPlayViewController;
 {
 	if( ! [super initWithNibName:@"PlayView" bundle:nil] ) return nil;
 	
+  AudioSessionInitialize (NULL, NULL, NULL, NULL); 
+  AudioSessionSetActive (true);
+  UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;
+  AudioSessionSetProperty (kAudioSessionProperty_AudioCategory, sizeof (sessionCategory), &sessionCategory);
+  
 	return self;
 }
 /*
